7fbc6112688614924fec2728fc119210b9807f18
mmaurirodriguez <mmaurirodriguez@udesa.edu.ar>
2025-04-09 23:03:51 -0300

    borre error en los controllers que hacia que crashee

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -19,22 +19,3 @@ const mainController = {
 module.exports = mainController;
 
 
-module.exports = {
-    // Ejemplos
-    index: function(req, res) {
-        
-        res.render('index', { title: 'Express', products , user, logeado: true });
-    },
-    login: function(req, res) {
-        res.render('login', { user } );
-    },
-    register: function(req, res) {
-        res.render('register', { user } );
-    },
-    search: function(req, res) {
-        res.render('search-results', { user } );
-    },
-    productDetail: function(req, res) {
-        res.render('product', { user } );
-    }
-};
--- controllers/productController.js
+++ controllers/productController.js
@@ -36,15 +36,3 @@ const productController= {
 module.exports = productController;
 
 
-module.exports = {
-    // Ejemplos
-    detail: function(req, res) {
-        res.render('product', { user } );
-    },
-    add: function(req, res) {
-        res.render('product-add', { user } );
-    },
-    create: function(req, res) {
-        res.redirect('/');
-    }
-};
