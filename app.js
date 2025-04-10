47dec333f166b1d931a6e6e5c79b557082b1feba
mmaurirodriguez <mmaurirodriguez@udesa.edu.ar>
2025-04-09 21:59:24 -0300

    corregi nombre de variables de la app.js

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var mainRouter = require('./routes/main');
+var userRouter = require('./routes/user');
+var productRouter = require('./routes/product');
 
 var app = express();
 
@@ -19,9 +20,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/products', require('./routes/products'));
+app.use('/', mainRouter);
+app.use('/user', userRouter);
+app.use('/product', productRouter);
 
 
 // catch 404 and forward to error handler
