c65fb1e41fdd73aac4d79dc5953db07aad856653
mmaurirodriguez <mmaurirodriguez@udesa.edu.ar>
2025-04-09 21:54:52 -0300

    corregi db

--- db/datos.js
+++ db/datos.js
@@ -1,41 +1,12 @@
 const datos = {
-    usuarios:[ {
-        email: "brian@mail.com",
-        usuario: "Brian",
+    usuarios: {
+        email: "mariana@mail.com",
+        usuario: "Mariana",
         password: "12345678",
         fechaNacimiento: "2006-04-10",
         documento: 45123456,
         fotoPerfil: "default-image.png"
       },
-      {
-        email: "olivia@mail.com",
-        usuario: "Olivia",
-        password: "12345678",
-        fechaNacimiento: "2005-10-14",
-        documento: 46123456,
-        fotoPerfil: "default-image.png"
-      },{
-        email: "mariana@mail.com",
-        usuario: "Mariana",
-        password: "12345678",
-        fechaNacimiento: "2006-06-12",
-        documento: 46234567,
-        fotoPerfil: "default-image.png"
-      },{
-        email: "ignacio@mail.com",
-        usuario: "Ignacio",
-        password: "12345678",
-        fechaNacimiento: "2005-03-07",
-        documento: 45234567,
-        fotoPerfil: "default-image.png"
-      },{
-        email: "luis@mail.com",
-        usuario: "Luis",
-        password: "12345678",
-        fechaNacimiento: "2003-08-20",
-        documento: 43123456,
-        fotoPerfil: "default-image.png"
-      }],
       productos: [
         {
             id: 1,
@@ -111,60 +82,65 @@ const datos = {
       comentarios:[
         {
             nombreUsuario: "brian123",
-            fotoUsuario: "default-image.png",
+            fotoUsuario: "/images/products/default-image.png",
             comentario: "Me encanta este modelo, siempre quise uno."
         },
         {
             nombreUsuario: "mariana123",
-            fotoUsuario: "default-image.png",
+            fotoUsuario: "/images/products/default-image.png",
             comentario: "Muy malo"
         },
         {
             nombreUsuario: "luli_rock",
-            fotoUsuario: "default-image.png",
+            fotoUsuario: "/images/products/default-image.png",
             comentario: "Lo compré y me sorprendió la calidad."
         },
         {
             nombreUsuario: "juanma_dev",
-            fotoUsuario: "default-image.png",
+            fotoUsuario: "/images/products/default-image.png",
             comentario: "Perfecto para lo que necesito. Recomendado."
         },
         {
             nombreUsuario: "sofi.gamer",
-            fotoUsuario: "default-image.png",
+            fotoUsuario: "/images/products/default-image.png",
             comentario: "Los colores y el diseño son lo mejor."
         },
         {
             nombreUsuario: "martinOk",
-            fotoUsuario: "default-image.png",
+            fotoUsuario: "/images/products/default-image.png",
             comentario: "Es justo lo que estaba buscando."
         },
         {
             nombreUsuario: "cami27",
-            fotoUsuario: "default-image.png",
+            fotoUsuario: "/images/products/default-image.png",
             comentario: "Súper cómodo y liviano, me encantó."
         },
         {
             nombreUsuario: "elias.tech",
-            fotoUsuario: "default-image.png",
+            fotoUsuario: "/images/products/default-image.png",
             comentario: "Rinde excelente para el trabajo diario."
         },
         {
             nombreUsuario: "juli_ramone",
-            fotoUsuario: "default-image.png",
+            fotoUsuario: "/images/products/default-image.png",
             comentario: "Muy buena atención y entrega rápida."
         },
         {
             nombreUsuario: "nati.design",
-            fotoUsuario: "default-image.png",
+            fotoUsuario: "/images/products/default-image.png",
             comentario: "Estéticamente hermoso, y muy funcional."
         }
 
       ],
-      filtrarId: function (id) {
-        if (this.productos[0].id==id) {
-            return this.productos[0]
+      filtrarID: function (idBuscado) {
+        let idEncontrado = [];
+        for (let i = 0; i < this.productos.length; i++) {
+            const element = this.productos[i];
+            if (element.id==idBuscado) {
+                idEncontrado.push(element)
+            }
         }
+        return idEncontrado
       }
 }
 module.exports = datos;
