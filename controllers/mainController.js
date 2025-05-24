0d9dea5f4b49e928eb61851e8f8324de7efec4c7
mmaurirodriguez <mmaurirodriguez@udesa.edu.ar>
2025-05-24 14:28:22 -0300

    cambios en los controllers

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,5 +1,6 @@
 const db = require('../database/models');
 const Producto = db.Producto;
+const Op = db.Sequelize.Op;
 
 const mainController = {
     index: function (req, res) {
@@ -10,8 +11,28 @@ const mainController = {
             return res.render("index", { productos });
         })
         .catch(error => res.send(error));
+    },
+
+    searchResults: function (req, res) {
+        const query = req.query.q || "";
+
+        Producto.findAll({
+            where: {
+                nombre: {
+                    [Op.like]: `%${query}%`
+                }
+            },
+            include: ['usuario']
+        })
+        .then(productos => {
+            return res.render("search-results", {
+                productos,
+                query
+            });
+        })
+        .catch(error => res.send(error));
     }
 };
-module.exports = mainController;
 
+module.exports = mainController;
 
--- controllers/productController.js
+++ controllers/productController.js
@@ -12,38 +12,31 @@ const productController = {
         .catch(error => res.send(error));
     },
 
-    detalle: function(req, res) {
+    filtrarID: function(req, res) {
         let idProducto = req.params.id;
 
         Producto.findByPk(idProducto, {
-            include: ['usuario'] // si querés mostrar quién lo subió
+            include: ['usuario', 'comentarios'] // si querés incluir comentarios también
         })
         .then(producto => {
             if (!producto) return res.send("Producto no encontrado");
-            return res.render("detalleProducto", { producto }); // Vista: detalleProducto.ejs
+
+          return res.render("product", {
+            detalle: [producto], // esto lo hacías antes, para manejar como array
+            infoComentarios: producto.comentarios || []
+        });
         })
         .catch(error => res.send(error));
     },
-
-    buscar: function(req, res) {
-        let keyword = req.query.search;
-
-        Producto.findAll({
-            where: {
-                nombre: {
-                    [Op.like]: `%${keyword}%`
-                }
-            },
-            include: ['usuario']
-        })
-        .then(productos => {
-            return res.render("search-results", {
-                productos,
-                keyword
-            });
-        })
-        .catch(error => res.send(error));
-    }
-};
-
+    productAdd: function(req, res) {
+    // si necesitás usuarios, cargalos
+    db.Usuario.findAll()
+    .then(usuarios => {
+        return res.render("productAdd", {
+            usuario: usuarios
+        });
+    })
+    .catch(error => res.send(error));
+}
+}
 module.exports = productController;
