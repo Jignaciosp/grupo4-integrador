7bce8a59d48e9c0c3c568554557b8f883ee343d1
“Jignaciosp“ <ignaciopassarini@gmail.com>
2025-05-31 21:29:23 -0300

    adentro de commetController realice el create

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,7 +1,16 @@
 const db = require('../database/models')
 
 const commentController = {
+    store: function(req,res) {
+        const {contenido, productId} = req.body;
 
+        db.Comentario.create{
+            comentario: contenido,
+            createAT: new Date(),
+            productId,
+            usuarioId: req.session.user.id
+        }
+    }
 }
 
 module.exports = commentController
