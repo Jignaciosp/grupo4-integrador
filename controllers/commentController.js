4c51fee1ff3b5203946062624cc57da856eea896
“Jignaciosp“ <ignaciopassarini@gmail.com>
2025-05-31 21:32:42 -0300

    Terminado commentController

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -4,14 +4,14 @@ const commentController = {
     store: function(req,res) {
         const {contenido, productId} = req.body;
 
-        db.Comentario.create{
+        db.Comentario.create({
             comentario: contenido,
             createAT: new Date(),
             productId,
             usuarioId: req.session.user.id
-        }
-        .then(() => res.redirect('/product/id/' + productId) )
-        
+        })
+        .then(() => res.redirect('/product/id/' + productId))
+        .catch(error => res.send("Error al crear comentario: " + errpr));
     }
 }
 
