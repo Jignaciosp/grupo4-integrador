19d128d96a7afc4fec6e80a4f2843878ece64a5a
mmaurirodriguez <mmaurirodriguez@udesa.edu.ar>
2025-06-09 18:30:48 -0300

    .

--- routes/product.js
+++ routes/product.js
@@ -3,16 +3,12 @@ var router = express.Router();
 const productController = require('../controllers/productController');// delego la logica al controller correspondiente
 
 
-function onlyLogged(req, res, next) {
-    if (!req.session.user) return res.redirect('/user/login');
-    next();
-}
 
 router.get('/', productController.index);
 
 router.get('/id/:id', productController.filtrarID);
-router.get('/add', onlyLogged, productController.productAdd);
-router.post('/create', onlyLogged, productController.store);
+router.get('/add', productController.productAdd);
+router.post('/create', productController.store);
 
 
 
